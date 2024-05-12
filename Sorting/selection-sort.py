6cbf36fef611e3837975ff62534e48f87da608c5
Vibhuti <imvibhuti364@gmail.com>
2024-05-12 11:34:07 +0530

    code

--- Sorting/selection-sort.py
+++ Sorting/selection-sort.py
@@ -7,19 +7,27 @@ Space complexity - O(1)
 
 def selection_sort(arr):
     n = len(arr)
-
+    k = 1
     # Traverse through all array elements
     for i in range(0, n):
+        print(f"case {k}")
         # Find the minimum element in the remaining unsorted array
         min_idx = i
+        print("min", min_idx)
         for j in range(i + 1, n):
+            print(f"{arr[j]} < {arr[min_idx]}")
             if arr[j] < arr[min_idx]:
                 min_idx = j
+                print("min_new", min_idx)
 
         # Swap the found minimum element with the first element
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
+        print(f"{arr[i]} = {arr[min_idx]}, {arr[min_idx]} = {{arr[i]}} ")
+        print(arr)
+        k += 1
 
 
-arr = [64, 25, 12, 22, 11]
+# arr = [64, 25, 12, 22, 11]
+arr = [5, 2, 4, 9, 1, 3, 10]
 selection_sort(arr)
 print(arr)
---
+++ dsa_practice/dll.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self, val=None) -> None:
+        self.val = val
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+
+    def insertAtHead(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+    def insertAtTail(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+    def traverse(self):
+        if not self.head:
+            print("DLL is empty")
+        else:
+            current = self.head
+            while current is not None:
+                print(current.val, end=" ")
+                current = current.next
+            print()
+
+    def deleteHead(self):
+        if not self.head:
+            print("List not Available")
+        else:
+            self.head = self.head.next
+        print()
+
+    def deleteTail(self):
+        if not self.head:
+            print("List not Available")
+        else:
+            self.tail = self.tail.prev
+        print()
+
+
+dll = DoublyLinkedList()
+dll.insertAtHead(100)
+dll.insertAtHead(200)
+dll.insertAtHead(300)
+dll.traverse()
+dll.deleteHead()
+dll.traverse()
+dll.deleteTail()
+dll.traverse()
+
+"""
+insertInBetween(2,987)
+deleteTail()
+deleteByValue(500)
+"""
